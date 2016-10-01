6da63092615d857aad0ddf16ebed9bb677ecbcd5
Johan Larsson <johan_gavle1@hotmail.com>
2016-10-01 20:57:36 +0200

    Amend.

--- WpfAnalyzers.Analyzers/DependencyProperties/WPF0001FieldMatchesRegisteredName.cs
+++ WpfAnalyzers.Analyzers/DependencyProperties/WPF0001FieldMatchesRegisteredName.cs
@@ -11,7 +11,7 @@
     internal class WPF0001FieldMatchesRegisteredName : DiagnosticAnalyzer
     {
         public const string DiagnosticId = "WPF0001";
-        private const string Title = "Backing field for a dependency property name must match registered name.";
+        private const string Title = "Backing field for a DependencyProperty must match registered name.";
         private const string MessageFormat = "Field '{0}' that is backing field for the DependencyProperty registered as '{1}' must be named '{1}Property'";
         private const string Description = "A dependency property's backing field must be named with the name it is registered with suffixed by 'Property'";
         private static readonly string HelpLink = WpfAnalyzers.HelpLink.ForId(DiagnosticId);
