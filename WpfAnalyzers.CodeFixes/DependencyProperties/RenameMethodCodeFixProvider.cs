8139252149fe735767e777d95e9334fdf748ffa6
Johan Larsson <johan_gavle1@hotmail.com>
2016-10-13 02:38:22 +0200

    Refactor: RenameMethodCodeFixProvider

--- WpfAnalyzers.CodeFixes/DependencyProperties/RenameMethodCodeFixProvider.cs
+++ WpfAnalyzers.CodeFixes/DependencyProperties/RenameMethodCodeFixProvider.cs
@@ -2,6 +2,7 @@
 {
     using System.Collections.Immutable;
     using System.Composition;
+    using System.Linq;
     using System.Threading.Tasks;
 
     using Microsoft.CodeAnalysis;
@@ -23,7 +24,8 @@
             var document = context.Document;
             var syntaxRoot = await document.GetSyntaxRootAsync(context.CancellationToken)
                                            .ConfigureAwait(false);
-
+            var semanticModel = await document.GetSemanticModelAsync(context.CancellationToken)
+                                             .ConfigureAwait(false);
             foreach (var diagnostic in context.Diagnostics)
             {
                 var token = syntaxRoot.FindToken(diagnostic.Location.SourceSpan.Start);
@@ -32,44 +34,70 @@
                     continue;
                 }
 
-                context.RegisterCodeFix(
-                    CodeAction.Create(
-                        "Rename property to match registered name.",
-                        _ => ApplyFixAsync(context, diagnostic),
-                        this.GetType().Name),
-                    diagnostic);
+                var methodDeclaration = syntaxRoot.FindNode(diagnostic.Location.SourceSpan)
+                                                  .FirstAncestorOrSelf<MethodDeclarationSyntax>();
+
+                IFieldSymbol backingField;
+                if (ClrMethod.IsAttachedSetMethod(
+                    methodDeclaration,
+                    semanticModel,
+                    context.CancellationToken,
+                    out backingField))
+                {
+                    TryUpdateName(
+                        context,
+                        backingField,
+                        semanticModel,
+                        syntaxRoot,
+                        token,
+                        "Set",
+                        diagnostic);
+
+                    continue;
+                }
+
+                if (ClrMethod.IsAttachedGetMethod(
+                    methodDeclaration,
+                    semanticModel,
+                    context.CancellationToken,
+                    out backingField))
+                {
+                    TryUpdateName(
+                        context,
+                        backingField,
+                        semanticModel,
+                        syntaxRoot,
+                        token,
+                        "Get",
+                        diagnostic);
+                }
             }
         }
 
-        private static async Task<Solution> ApplyFixAsync(CodeFixContext context, Diagnostic diagnostic)
+        private static void TryUpdateName(
+            CodeFixContext context,
+            IFieldSymbol setField,
+            SemanticModel semanticModel,
+            SyntaxNode syntaxRoot,
+            SyntaxToken token,
+            string prefix,
+            Diagnostic diagnostic)
         {
-            var document = context.Document;
-            var syntaxRoot = await document.GetSyntaxRootAsync(context.CancellationToken)
-                                           .ConfigureAwait(false);
-            var semanticModel = await document.GetSemanticModelAsync(context.CancellationToken)
-                                              .ConfigureAwait(false);
-
-            var token = syntaxRoot.FindToken(diagnostic.Location.SourceSpan.Start);
-            var method = syntaxRoot.FindNode(diagnostic.Location.SourceSpan)
-                                        .FirstAncestorOrSelf<MethodDeclarationSyntax>();
-
             string registeredName;
-            string expectedName = null;
-            if (method.TryGetDependencyPropertyRegisteredNameFromAttachedGet(semanticModel, context.CancellationToken, out registeredName))
+            if (DependencyProperty.TryGetRegisteredName(
+                setField,
+                semanticModel,
+                context.CancellationToken,
+                out registeredName))
             {
-                expectedName = "Get" + registeredName;
+                var newName = $"{prefix}{registeredName}";
+                context.RegisterCodeFix(
+                    CodeAction.Create(
+                        $"Rename to: {newName}",
+                        cancellationToken => RenameHelper.RenameSymbolAsync(context.Document, syntaxRoot, token, newName, cancellationToken),
+                        nameof(RenameMethodCodeFixProvider)),
+                    diagnostic);
             }
-            else if (method.TryGetDependencyPropertyRegisteredNameFromAttachedSet(semanticModel, context.CancellationToken, out registeredName))
-            {
-                expectedName = "Set" + registeredName;
-            }
-
-            if (expectedName != null)
-            {
-                return await RenameHelper.RenameSymbolAsync(document, syntaxRoot, token, expectedName, context.CancellationToken).ConfigureAwait(false);
-            }
-
-            return document.Project.Solution;
         }
     }
 }
