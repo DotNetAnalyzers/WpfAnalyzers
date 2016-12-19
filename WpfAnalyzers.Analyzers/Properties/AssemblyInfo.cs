d2e9a4d2640b463220b650ebbb2cac796c9d48b6
Johan Larsson <johan_gavle1@hotmail.com>
2016-12-19 14:20:18 +0100

    Bump version.

--- WpfAnalyzers.Analyzers/Properties/AssemblyInfo.cs
+++ WpfAnalyzers.Analyzers/Properties/AssemblyInfo.cs
@@ -15,9 +15,9 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.2.5.0")]
-[assembly: AssemblyFileVersion("0.2.5.0")]
-[assembly: AssemblyInformationalVersion("0.2.5.0-dev")]
+[assembly: AssemblyVersion("0.2.5.1")]
+[assembly: AssemblyFileVersion("0.2.5.1")]
+[assembly: AssemblyInformationalVersion("0.2.5.1-dev")]
 
 [assembly: InternalsVisibleTo("WpfAnalyzers.CodeFixes")]
 [assembly: InternalsVisibleTo("WpfAnalyzers.Test")]
