9bf32022b10e8a21facfb0037a1eebd0de21c3f0
Johan Larsson <johan_gavle1@hotmail.com>
2016-12-21 12:19:13 +0100

    Handle SemanticModel being null

--- WpfAnalyzers.Analyzers/Helpers/SemanticModelExt.cs
+++ WpfAnalyzers.Analyzers/Helpers/SemanticModelExt.cs
@@ -18,56 +18,56 @@
         internal static ISymbol GetSymbolSafe(this SemanticModel semanticModel, SyntaxNode node, CancellationToken cancellationToken)
         {
             return semanticModel.SemanticModelFor(node)
-                                .GetSymbolInfo(node, cancellationToken)
+                                ?.GetSymbolInfo(node, cancellationToken)
                                 .Symbol;
         }
 
         internal static IFieldSymbol GetDeclaredSymbolSafe(this SemanticModel semanticModel, FieldDeclarationSyntax node, CancellationToken cancellationToken)
         {
             return (IFieldSymbol)semanticModel.SemanticModelFor(node)
-                                               .GetDeclaredSymbol(node, cancellationToken);
+                                              ?.GetDeclaredSymbol(node, cancellationToken);
         }
 
         internal static IMethodSymbol GetDeclaredSymbolSafe(this SemanticModel semanticModel, ConstructorDeclarationSyntax node, CancellationToken cancellationToken)
         {
             return (IMethodSymbol)semanticModel.SemanticModelFor(node)
-                                               .GetDeclaredSymbol(node, cancellationToken);
+                                               ?.GetDeclaredSymbol(node, cancellationToken);
         }
 
         internal static IPropertySymbol GetDeclaredSymbolSafe(this SemanticModel semanticModel, PropertyDeclarationSyntax node, CancellationToken cancellationToken)
         {
             return (IPropertySymbol)semanticModel.SemanticModelFor(node)
-                                               .GetDeclaredSymbol(node, cancellationToken);
+                                                ?.GetDeclaredSymbol(node, cancellationToken);
         }
 
         internal static IMethodSymbol GetDeclaredSymbolSafe(this SemanticModel semanticModel, MethodDeclarationSyntax node, CancellationToken cancellationToken)
         {
             return (IMethodSymbol)semanticModel.SemanticModelFor(node)
-                                               .GetDeclaredSymbol(node, cancellationToken);
+                                               ?.GetDeclaredSymbol(node, cancellationToken);
         }
 
         internal static ITypeSymbol GetDeclaredSymbolSafe(this SemanticModel semanticModel, TypeDeclarationSyntax node, CancellationToken cancellationToken)
         {
             return (ITypeSymbol)semanticModel.SemanticModelFor(node)
-                                             .GetDeclaredSymbol(node, cancellationToken);
+                                             ?.GetDeclaredSymbol(node, cancellationToken);
         }
 
         internal static ISymbol GetDeclaredSymbolSafe(this SemanticModel semanticModel, SyntaxNode node, CancellationToken cancellationToken)
         {
             return semanticModel.SemanticModelFor(node)
-                                .GetDeclaredSymbol(node, cancellationToken);
+                                ?.GetDeclaredSymbol(node, cancellationToken);
         }
 
         internal static Optional<object> GetConstantValueSafe(this SemanticModel semanticModel, SyntaxNode node, CancellationToken cancellationToken)
         {
             return semanticModel.SemanticModelFor(node)
-                                .GetConstantValue(node, cancellationToken);
+                                ?.GetConstantValue(node, cancellationToken) ?? default(Optional<object>);
         }
 
         internal static TypeInfo GetTypeInfoSafe(this SemanticModel semanticModel, SyntaxNode node, CancellationToken cancellationToken)
         {
             return semanticModel.SemanticModelFor(node)
-                                .GetTypeInfo(node, cancellationToken);
+                                ?.GetTypeInfo(node, cancellationToken) ?? default(TypeInfo);
         }
 
         /// <summary>
