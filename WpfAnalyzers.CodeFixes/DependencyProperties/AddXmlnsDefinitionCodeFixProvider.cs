c64412714ef04b0a32b5356dd43c9c9184434fa1
Johan Larsson <johan_gavle1@hotmail.com>
2016-12-29 16:11:51 +0100

    WPF0052 map all namespaces with public types. Fix #14.

---
+++ WpfAnalyzers.CodeFixes/DependencyProperties/AddXmlnsDefinitionCodeFixProvider.cs
@@ -0,0 +1,70 @@
+﻿namespace WpfAnalyzers.DependencyProperties
+{
+    using System.Collections.Immutable;
+    using System.Composition;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CodeActions;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Formatting;
+
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AddXmlnsDefinitionCodeFixProvider))]
+    [Shared]
+    internal class AddXmlnsDefinitionCodeFixProvider : CodeFixProvider
+    {
+        /// <inheritdoc/>
+        public override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create(WPF0052XmlnsDefinitionsDoesNotMapAllNamespaces.DiagnosticId);
+
+        /// <inheritdoc/>
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var syntaxRoot = await context.Document.GetSyntaxRootAsync(context.CancellationToken)
+                              .ConfigureAwait(false);
+            foreach (var diagnostic in context.Diagnostics)
+            {
+                var token = syntaxRoot.FindToken(diagnostic.Location.SourceSpan.Start);
+                if (string.IsNullOrEmpty(token.ValueText))
+                {
+                    continue;
+                }
+
+                var attribute = syntaxRoot.FindNode(diagnostic.Location.SourceSpan)
+                                     .FirstAncestorOrSelf<AttributeSyntax>();
+                if (attribute == null || attribute.IsMissing)
+                {
+                    continue;
+                }
+
+                var toAdd = diagnostic.Properties.Values.Select(x => CreateAttribute(x, attribute))
+                                      .Where(a => a != null)
+                                      .ToArray();
+                if (toAdd.Any())
+                {
+                    context.RegisterCodeFix(
+                    CodeAction.Create(
+                        "Map missing namespaces.",
+                        _ => Task.FromResult(context.Document.WithSyntaxRoot(syntaxRoot.InsertNodesAfter(attribute.FirstAncestorOrSelf<AttributeListSyntax>(), toAdd))),
+                        this.GetType().FullName),
+                    diagnostic);
+                }
+            }
+        }
+
+        private static AttributeListSyntax CreateAttribute(string @namespace, AttributeSyntax attribute)
+        {
+            var list = attribute.FirstAncestorOrSelf<AttributeListSyntax>();
+            if (list?.Attributes.Count != 1 || attribute.ArgumentList.Arguments.Count != 2)
+            {
+                return null;
+            }
+
+            var oldArgument = attribute.ArgumentList.Arguments[1];
+            var newArgument = oldArgument.WithExpression(SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal(@namespace)));
+            return list.WithAttributes(SyntaxFactory.SingletonSeparatedList(attribute.ReplaceNode(oldArgument, newArgument))).WithAdditionalAnnotations(Formatter.Annotation);
+        }
+    }
+}
---
+++ WpfAnalyzers.Test/DependencyProperties/WPF0052XmlnsDefinitionsDoesNotMapAllNamespacesTests/HappyPath.cs
@@ -0,0 +1,246 @@
+﻿namespace WpfAnalyzers.Test.DependencyProperties.WPF0052XmlnsDefinitionsDoesNotMapAllNamespacesTests
+{
+    using System.Threading.Tasks;
+
+    using NUnit.Framework;
+
+    using WpfAnalyzers.DependencyProperties;
+
+    internal class HappyPath : HappyPathVerifier<WPF0052XmlnsDefinitionsDoesNotMapAllNamespaces>
+    {
+        [Test]
+        public async Task WhenXmlnsDefinitionMatches()
+        {
+            var controlCode = @"namespace Gu.Wpf.Geometry
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    public class FooControl : Control
+    {
+        public static readonly DependencyProperty BrushProperty = DependencyProperty.Register(
+            nameof(Brush),
+            typeof(Brush),
+            typeof(FooControl),
+            new PropertyMetadata(default(Brush)));
+
+        public Brush Brush
+        {
+            get { return (Brush)this.GetValue(BrushProperty); }
+            set { this.SetValue(BrushProperty, value); }
+        }
+    }
+}";
+            var testCode = @"
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: AssemblyTitle(""Gu.Wpf.Geometry"")]
+[assembly: AssemblyDescription(""Geometries for WPF."")]
+[assembly: AssemblyConfiguration("""")]
+[assembly: AssemblyCompany(""Johan Larsson"")]
+[assembly: AssemblyProduct(""Gu.Wpf.Geometry"")]
+[assembly: AssemblyCopyright(""Copyright ©  2015"")]
+[assembly: AssemblyTrademark("""")]
+[assembly: AssemblyCulture("""")]
+[assembly: ComVisible(false)]
+[assembly: Guid(""a9cbadf5-65f8-4a81-858c-ea0cdfeb2e99"")]
+[assembly: AssemblyVersion(""1.0.0.2"")]
+[assembly: AssemblyFileVersion(""1.0.0.2"")]
+[assembly: InternalsVisibleTo(""Gu.Wpf.Geometry.Tests"", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo(""Gu.Wpf.Geometry.Demo"", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo(""Gu.Wpf.Geometry.Benchmarks"", AllInternalsVisible = true)]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition(""http://gu.se/Geometry"", ""Gu.Wpf.Geometry"")]";
+            await this.VerifyHappyPathAsync(new[] { testCode, controlCode }).ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task WhenTwoPublicTypesInSameNamespace()
+        {
+            var control1Code = @"namespace Gu.Wpf.Geometry
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    public class FooControl : Control
+    {
+        public static readonly DependencyProperty BrushProperty = DependencyProperty.Register(
+            nameof(Brush),
+            typeof(Brush),
+            typeof(FooControl),
+            new PropertyMetadata(default(Brush)));
+
+        public Brush Brush
+        {
+            get { return (Brush)this.GetValue(BrushProperty); }
+            set { this.SetValue(BrushProperty, value); }
+        }
+    }
+}";
+
+            var control2Code = @"namespace Gu.Wpf.Geometry
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    public class BarControl : Control
+    {
+        public static readonly DependencyProperty BrushProperty = DependencyProperty.Register(
+            nameof(Brush),
+            typeof(Brush),
+            typeof(BarControl),
+            new PropertyMetadata(default(Brush)));
+
+        public Brush Brush
+        {
+            get { return (Brush)this.GetValue(BrushProperty); }
+            set { this.SetValue(BrushProperty, value); }
+        }
+    }
+}";
+
+            var testCode = @"
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: AssemblyTitle(""Gu.Wpf.Geometry"")]
+[assembly: AssemblyDescription(""Geometries for WPF."")]
+[assembly: AssemblyConfiguration("""")]
+[assembly: AssemblyCompany(""Johan Larsson"")]
+[assembly: AssemblyProduct(""Gu.Wpf.Geometry"")]
+[assembly: AssemblyCopyright(""Copyright ©  2015"")]
+[assembly: AssemblyTrademark("""")]
+[assembly: AssemblyCulture("""")]
+[assembly: ComVisible(false)]
+[assembly: Guid(""a9cbadf5-65f8-4a81-858c-ea0cdfeb2e99"")]
+[assembly: AssemblyVersion(""1.0.0.2"")]
+[assembly: AssemblyFileVersion(""1.0.0.2"")]
+[assembly: InternalsVisibleTo(""Gu.Wpf.Geometry.Tests"", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo(""Gu.Wpf.Geometry.Demo"", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo(""Gu.Wpf.Geometry.Benchmarks"", AllInternalsVisible = true)]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition(""http://gu.se/Geometry"", ""Gu.Wpf.Geometry"")]";
+            await this.VerifyHappyPathAsync(new[] { testCode, control1Code, control2Code }).ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task WhenTwoXmlnsDefinitions()
+        {
+            var controlCode1 = @"namespace Gu.Wpf.Geometry
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    public class FooControl1 : Control
+    {
+        public static readonly DependencyProperty BrushProperty = DependencyProperty.Register(
+            nameof(Brush),
+            typeof(Brush),
+            typeof(FooControl1),
+            new PropertyMetadata(default(Brush)));
+
+        public Brush Brush
+        {
+            get { return (Brush)this.GetValue(BrushProperty); }
+            set { this.SetValue(BrushProperty, value); }
+        }
+    }
+}";
+
+            var controlCode2 = @"namespace Gu.Wpf.Geometry.Balloons
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    public class FooControl2 : Control
+    {
+        public static readonly DependencyProperty BrushProperty = DependencyProperty.Register(
+            nameof(Brush),
+            typeof(Brush),
+            typeof(FooControl2),
+            new PropertyMetadata(default(Brush)));
+
+        public Brush Brush
+        {
+            get { return (Brush)this.GetValue(BrushProperty); }
+            set { this.SetValue(BrushProperty, value); }
+        }
+    }
+}";
+
+            var testCode = @"
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: AssemblyTitle(""Gu.Wpf.Geometry"")]
+[assembly: AssemblyDescription(""Geometries for WPF."")]
+[assembly: AssemblyConfiguration("""")]
+[assembly: AssemblyCompany(""Johan Larsson"")]
+[assembly: AssemblyProduct(""Gu.Wpf.Geometry"")]
+[assembly: AssemblyCopyright(""Copyright ©  2015"")]
+[assembly: AssemblyTrademark("""")]
+[assembly: AssemblyCulture("""")]
+[assembly: ComVisible(false)]
+[assembly: Guid(""a9cbadf5-65f8-4a81-858c-ea0cdfeb2e99"")]
+[assembly: AssemblyVersion(""1.0.0.2"")]
+[assembly: AssemblyFileVersion(""1.0.0.2"")]
+[assembly: InternalsVisibleTo(""Gu.Wpf.Geometry.Tests"", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo(""Gu.Wpf.Geometry.Demo"", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo(""Gu.Wpf.Geometry.Benchmarks"", AllInternalsVisible = true)]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition(""http://gu.se/Geometry"", ""Gu.Wpf.Geometry"")]
+[assembly: XmlnsDefinition(""http://gu.se/Geometry"", ""Gu.Wpf.Geometry.Balloons"")]";
+
+            await this.VerifyHappyPathAsync(new[] { testCode, controlCode1, controlCode2 }).ConfigureAwait(false);
+        }
+
+        [Test]
+        public async Task WhenNoNamespace()
+        {
+            var testCode = @"
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: AssemblyTitle(""Gu.Wpf.Geometry"")]
+[assembly: AssemblyDescription(""Geometries for WPF."")]
+[assembly: AssemblyConfiguration("""")]
+[assembly: AssemblyCompany(""Johan Larsson"")]
+[assembly: AssemblyProduct(""Gu.Wpf.Geometry"")]
+[assembly: AssemblyCopyright(""Copyright ©  2015"")]
+[assembly: AssemblyTrademark("""")]
+[assembly: AssemblyCulture("""")]
+[assembly: ComVisible(false)]
+[assembly: Guid(""a9cbadf5-65f8-4a81-858c-ea0cdfeb2e99"")]
+[assembly: AssemblyVersion(""1.0.0.2"")]
+[assembly: AssemblyFileVersion(""1.0.0.2"")]
+[assembly: InternalsVisibleTo(""Gu.Wpf.Geometry.Tests"", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo(""Gu.Wpf.Geometry.Demo"", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo(""Gu.Wpf.Geometry.Benchmarks"", AllInternalsVisible = true)]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition(""http://gu.se/Geometry"", ""Gu.Wpf.Geometry"")]";
+            await this.VerifyHappyPathAsync(testCode).ConfigureAwait(false);
+        }
+    }
+}
