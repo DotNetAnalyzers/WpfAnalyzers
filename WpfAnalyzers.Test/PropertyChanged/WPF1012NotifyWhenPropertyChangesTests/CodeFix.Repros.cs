26c9b45e9ff611cb1217f4976dc64bd9efa1a137
Johan Larsson <johan_gavle1@hotmail.com>
2017-04-12 21:05:27 +0200

    Another test, no repro.

--- WpfAnalyzers.Test/PropertyChanged/WPF1012NotifyWhenPropertyChangesTests/CodeFix.Repros.cs
+++ WpfAnalyzers.Test/PropertyChanged/WPF1012NotifyWhenPropertyChangesTests/CodeFix.Repros.cs
@@ -107,5 +107,108 @@ namespace Vanguard_MVVM.ViewModels
                       .ConfigureAwait(false);
         }
 
+        [Test]
+        public async Task Vanguard_MVVM_ViewModels_MainWindowViewModelCommentedOut()
+        {
+            var childDataContext = @"namespace Vanguard_MVVM.Infrastructure
+{
+    public interface IChildDataContext
+    {
+        string Title { get; }
+    }
+}";
+            var testCode = @"
+namespace Vanguard_MVVM.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Infrastructure;
+
+
+    public sealed class MainWindowViewModel : INotifyPropertyChanged
+    {
+        IChildDataContext _childDataContext;
+        readonly string _title;
+        MainWindowViewModel()
+        {
+            _title = ""MVVM Attempt"";
+        }
+
+        public IChildDataContext ChildDataContext
+        {
+            get { return _childDataContext; }
+
+            private set
+            {
+                if (Equals(value, _childDataContext)) return;
+                ↓_childDataContext = value;
+                NotifyPropertyChanged(nameof(ChildDataContext));
+                //NotifyPropertyChanged(nameof(Title));
+            }
+        }
+
+        public static MainWindowViewModel Instance { get; } = new MainWindowViewModel();
+
+        public string Title => ChildDataContext?.Title == null ? _title : string.Concat(_title, "" - "", ChildDataContext?.Title);
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void NotifyPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    }
+}";
+
+            var expected = this.CSharpDiagnostic().WithLocationIndicated(ref testCode).WithArguments("Title");
+            await this.VerifyCSharpDiagnosticAsync(new[] { childDataContext, testCode }, expected).ConfigureAwait(false);
+
+            var fixedCode = @"
+namespace Vanguard_MVVM.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Infrastructure;
+
+
+    public sealed class MainWindowViewModel : INotifyPropertyChanged
+    {
+        IChildDataContext _childDataContext;
+        readonly string _title;
+        MainWindowViewModel()
+        {
+            _title = ""MVVM Attempt"";
+        }
+
+        public IChildDataContext ChildDataContext
+        {
+            get { return _childDataContext; }
+
+            private set
+            {
+                if (Equals(value, _childDataContext)) return;
+                _childDataContext = value;
+                NotifyPropertyChanged(nameof(ChildDataContext));
+                NotifyPropertyChanged(nameof(Title));
+                //NotifyPropertyChanged(nameof(Title));
+            }
+        }
+
+        public static MainWindowViewModel Instance { get; } = new MainWindowViewModel();
+
+        public string Title => ChildDataContext?.Title == null ? _title : string.Concat(_title, "" - "", ChildDataContext?.Title);
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void NotifyPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    }
+}";
+
+            await this.VerifyCSharpFixAsync(new[] { childDataContext, testCode }, new[] { childDataContext, fixedCode }, allowNewCompilerDiagnostics: true)
+                      .ConfigureAwait(false);
+        }
     }
 }
