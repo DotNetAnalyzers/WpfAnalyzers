51a001b18d7ee44c44a8e0c6ed963a74ce2a8a8e
Johan Larsson <johan_gavle1@hotmail.com>
2017-10-21 13:32:04 +0200

    Refactor WPF0006 & WPF0007

--- WpfAnalyzers.Analyzers/WPF0006CoerceValueCallbackShouldMatchRegisteredName.cs
+++ WpfAnalyzers.Analyzers/WPF0006CoerceValueCallbackShouldMatchRegisteredName.cs
@@ -31,52 +31,51 @@
         {
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
-            context.RegisterSyntaxNodeAction(HandleObjectCreation, SyntaxKind.ObjectCreationExpression);
+            context.RegisterSyntaxNodeAction(Handle, SyntaxKind.ObjectCreationExpression);
         }
 
         internal static bool TryGetIdentifierAndRegisteredName(ArgumentSyntax callback, SemanticModel semanticModel, CancellationToken cancellationToken, out IdentifierNameSyntax identifier, out string registeredName)
         {
             registeredName = null;
-            if (!CoerceValueCallback.TryGetName(callback, semanticModel, cancellationToken, out identifier, out string _))
-            {
-                return false;
-            }
-
-            return CoerceValueCallback.TryGetRegisteredName(callback, semanticModel, cancellationToken, out registeredName);
+            return CoerceValueCallback.TryGetName(callback, semanticModel, cancellationToken, out identifier, out string _) &&
+                   CoerceValueCallback.TryGetRegisteredName(callback, semanticModel, cancellationToken, out registeredName);
         }
 
-        private static void HandleObjectCreation(SyntaxNodeAnalysisContext context)
+        private static void Handle(SyntaxNodeAnalysisContext context)
         {
             if (context.IsExcludedFromAnalysis())
             {
                 return;
             }
 
-            var objectCreation = context.Node as ObjectCreationExpressionSyntax;
-            if (objectCreation == null || objectCreation.IsMissing)
+            if (context.Node is ObjectCreationExpressionSyntax objectCreation &&
+                context.ContainingSymbol.IsStatic)
             {
-                return;
-            }
-
-            if (!PropertyMetaData.TryGetCoerceValueCallback(
-        objectCreation,
-        context.SemanticModel,
-        context.CancellationToken,
-        out ArgumentSyntax callback))
-            {
-                return;
-            }
-
-            if (TryGetIdentifierAndRegisteredName(
-callback,
-context.SemanticModel,
-context.CancellationToken,
-out IdentifierNameSyntax nameExpression,
-out string registeredName))
-            {
-                if (!nameExpression.Identifier.ValueText.IsParts("Coerce", registeredName))
+                if (!PropertyMetaData.TryGetCoerceValueCallback(
+                    objectCreation,
+                    context.SemanticModel,
+                    context.CancellationToken,
+                    out var callback))
                 {
-                    context.ReportDiagnostic(Diagnostic.Create(Descriptor, nameExpression.GetLocation(), nameExpression, $"Coerce{registeredName}"));
+                    return;
+                }
+
+                if (TryGetIdentifierAndRegisteredName(
+                    callback,
+                    context.SemanticModel,
+                    context.CancellationToken,
+                    out var nameExpression,
+                    out var registeredName))
+                {
+                    if (!nameExpression.Identifier.ValueText.IsParts("Coerce", registeredName))
+                    {
+                        context.ReportDiagnostic(
+                            Diagnostic.Create(
+                                Descriptor,
+                                nameExpression.GetLocation(),
+                                nameExpression,
+                                $"Coerce{registeredName}"));
+                    }
                 }
             }
         }
