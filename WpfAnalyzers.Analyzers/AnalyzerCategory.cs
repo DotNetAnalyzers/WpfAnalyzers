1847e8c4bfffda121a85d2df7a9def0d9dc0a7a8
Johan Larsson <johan_gavle1@hotmail.com>
2017-11-21 23:40:53 +0100

    Fix warning.

--- WpfAnalyzers.Analyzers/AnalyzerCategory.cs
+++ WpfAnalyzers.Analyzers/AnalyzerCategory.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+// ReSharper disable InconsistentNaming
 namespace WpfAnalyzers
 {
     /// <summary>
