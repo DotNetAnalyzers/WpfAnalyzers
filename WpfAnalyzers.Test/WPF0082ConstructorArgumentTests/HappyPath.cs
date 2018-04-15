24ce3851d8c63220fd6423e1ea8442be17c35406
JohanLarsson <johan_gavle1@hotmail.com>
2018-04-15 12:15:55 +0200

    Test for #185, no repro.

--- WpfAnalyzers.Test/WPF0082ConstructorArgumentTests/HappyPath.cs
+++ WpfAnalyzers.Test/WPF0082ConstructorArgumentTests/HappyPath.cs
@@ -182,5 +182,101 @@ namespace RoslynSandbox
 }";
             AnalyzerAssert.Valid(Analyzer, testCode);
         }
+
+        [Test]
+        public void Issue185WithEnsure()
+        {
+            var ensureCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.Diagnostics;
+    using System.Runtime.CompilerServices;
+
+    public static class Ensure
+    {
+        public static T NotNull<T>(T value, string parameter, [CallerMemberName] string caller = null)
+            where T : class
+        {
+            Debug.Assert(!string.IsNullOrEmpty(parameter), ""parameter cannot be null"");
+
+            if (value == null)
+            {
+                var message = $""Expected parameter {parameter} in member {caller} to not be null"";
+                throw new ArgumentNullException(parameter, message);
+            }
+
+            return value;
+        }
+
+        internal static void IsTrue(bool condition, string parameterName, string message)
+        {
+            Debug.Assert(!string.IsNullOrEmpty(parameterName), $""{nameof(parameterName)} cannot be null"");
+            if (!condition)
+            {
+                if (!string.IsNullOrEmpty(message))
+                {
+                    throw new ArgumentException(message, parameterName);
+                }
+                else
+                {
+                    throw new ArgumentException(parameterName);
+                }
+            }
+        }
+    }
+}";
+
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.Windows.Markup;
+
+    /// <summary>
+    /// Markupextension for getting Enum.GetValues(this.Type)
+    /// </summary>
+    [MarkupExtensionReturnType(typeof(Array))]
+    public class EnumValuesForExtension : MarkupExtension
+    {
+        private Type type;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref=""EnumValuesForExtension""/> class.
+        /// </summary>
+        /// <param name=""type"">The enum type.</param>
+        public EnumValuesForExtension(Type type)
+        {
+            Ensure.IsTrue(type.IsEnum, nameof(type), ""Expected type to be an enum"");
+            this.type = type;
+        }
+
+        /// <summary>
+        /// The enum type.
+        /// </summary>
+        [ConstructorArgument(""type"")]
+        public Type Type
+        {
+            get => this.type;
+
+            set
+            {
+                Ensure.NotNull(value, nameof(value));
+                Ensure.IsTrue(value.IsEnum, nameof(value), $""Expected {this.Type} to be an enum."");
+                this.type = value;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            Ensure.NotNull(this.Type, nameof(this.Type));
+            return Enum.GetValues(this.Type);
+        }
+    }
+}";
+
+            AnalyzerAssert.Valid(Analyzer, ensureCode, testCode);
+        }
     }
 }
