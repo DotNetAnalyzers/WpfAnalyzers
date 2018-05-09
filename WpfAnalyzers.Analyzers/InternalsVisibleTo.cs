79af3056e99236c8fb1442d86d6c1a8ad9e3bc46
JohanLarsson <johan_gavle1@hotmail.com>
2018-05-09 12:55:09 +0200

    Setup pack with extensions.

--- WpfAnalyzers.Analyzers/InternalsVisibleTo.cs
+++ WpfAnalyzers.Analyzers/InternalsVisibleTo.cs
@@ -1,22 +1,4 @@
-﻿using System;
-using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("WpfAnalyzers.Analyzers")]
-[assembly: AssemblyDescription("Roslyn analyzers for WPF.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Johan Larsson")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: CLSCompliant(false)]
-[assembly: NeutralResourcesLanguage("en-US")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.1.2.1")]
-[assembly: AssemblyFileVersion("2.1.2.1")]
 
 [assembly: InternalsVisibleTo("WpfAnalyzers.CodeFixes, PublicKey=0024000004800000940000000602000000240000525341310004000001000100C160F349727AA71B3E828AC41FFFC5BF78685702D1B0FF1341ADC1CC10B37A27FFF3B530F9B3F276D83391364D23AE95D182C31EFFAEEBFB409641A260DDBF5A4E226EC0E02A1B854046DDBF1884DDB8ABDEBF9AD67C9C848F69BD9468F594BF03D62DE0B8B8FD7FC2EB5241A9165F32EC1417E5020E19FCB98E36097BDCFCB7")]
 [assembly: InternalsVisibleTo("WpfAnalyzers.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100C160F349727AA71B3E828AC41FFFC5BF78685702D1B0FF1341ADC1CC10B37A27FFF3B530F9B3F276D83391364D23AE95D182C31EFFAEEBFB409641A260DDBF5A4E226EC0E02A1B854046DDBF1884DDB8ABDEBF9AD67C9C848F69BD9468F594BF03D62DE0B8B8FD7FC2EB5241A9165F32EC1417E5020E19FCB98E36097BDCFCB7")]
