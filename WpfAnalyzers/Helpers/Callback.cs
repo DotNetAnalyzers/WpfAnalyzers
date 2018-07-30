7225629b8c15b92c8a40e34d2ae0768ac39d31e7
JohanLarsson <johan_gavle1@hotmail.com>
2018-07-30 08:47:34 +0200

    Refactor Callback

--- WpfAnalyzers/Helpers/Callback.cs
+++ WpfAnalyzers/Helpers/Callback.cs
@@ -1,5 +1,6 @@
 namespace WpfAnalyzers
 {
+    using System;
     using System.Threading;
     using Gu.Roslyn.AnalyzerExtensions;
     using Microsoft.CodeAnalysis;
@@ -7,27 +8,52 @@ namespace WpfAnalyzers
 
     internal static class Callback
     {
-        internal static bool TryGetName(ArgumentSyntax callback, QualifiedType callbackSymbol, SemanticModel semanticModel, CancellationToken cancellationToken, out IdentifierNameSyntax nameExpression, out string name)
+        internal static bool TryGetTarget(ArgumentSyntax callback, QualifiedType callbackSymbol, SemanticModel semanticModel, CancellationToken cancellationToken, out IdentifierNameSyntax identifier, out IMethodSymbol method)
         {
-            nameExpression = null;
-            name = null;
+            identifier = null;
+            method = null;
 
             if (callback == null)
             {
                 return false;
             }
 
-            if (callback.Expression is IdentifierNameSyntax identifierNameSyntax)
+            if (callback.Expression is IdentifierNameSyntax candidate &&
+                semanticModel.TryGetSymbol(candidate, cancellationToken, out method))
             {
-                nameExpression = identifierNameSyntax;
-                name = identifierNameSyntax.Identifier.ValueText;
+                identifier = candidate;
                 return true;
             }
 
             return callback.Expression is ObjectCreationExpressionSyntax creation &&
                    semanticModel.GetTypeInfoSafe(creation, cancellationToken).Type == callbackSymbol &&
                    creation.ArgumentList.Arguments.TrySingle(out var arg) &&
-                   TryGetName(arg, callbackSymbol, semanticModel, cancellationToken, out nameExpression, out name);
+                   TryGetTarget(arg, callbackSymbol, semanticModel, cancellationToken, out identifier, out method);
+        }
+
+        [Obsolete("Don't use this.")]
+        internal static bool TryGetName(ArgumentSyntax callback, QualifiedType callbackSymbol, SemanticModel semanticModel, CancellationToken cancellationToken, out IdentifierNameSyntax nameExpression, out string name)
+        {
+            if (TryGetTarget(callback, callbackSymbol, semanticModel, cancellationToken, out nameExpression, out var target))
+            {
+                name = target.Name;
+                return true;
+            }
+
+            name = null;
+            return false;
+        }
+
+        internal static bool IsSingleExpression(MethodDeclarationSyntax method)
+        {
+            if (method.ExpressionBody != null)
+            {
+                return true;
+            }
+
+            return method.Body is BlockSyntax body &&
+                   body.Statements.TrySingle(out var statement) &&
+                   statement is ExpressionStatementSyntax;
         }
     }
 }
