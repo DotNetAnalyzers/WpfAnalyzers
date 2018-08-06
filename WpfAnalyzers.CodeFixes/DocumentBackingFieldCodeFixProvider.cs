8c4cec3001716f321f4372d2bb4df970bd00cfc2
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-06 19:23:51 +0200

    Use WithDocumentationText

--- WpfAnalyzers.CodeFixes/DocumentBackingFieldCodeFixProvider.cs
+++ WpfAnalyzers.CodeFixes/DocumentBackingFieldCodeFixProvider.cs
@@ -2,15 +2,12 @@ namespace WpfAnalyzers
 {
     using System.Collections.Immutable;
     using System.Composition;
-    using System.Threading;
     using System.Threading.Tasks;
     using Gu.Roslyn.AnalyzerExtensions;
     using Gu.Roslyn.CodeFixExtensions;
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CodeFixes;
-    using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
-    using Microsoft.CodeAnalysis.Editing;
 
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(DocumentBackingFieldCodeFixProvider))]
     [Shared]
@@ -26,6 +23,8 @@ namespace WpfAnalyzers
             var document = context.Document;
             var syntaxRoot = await document.GetSyntaxRootAsync(context.CancellationToken)
                                            .ConfigureAwait(false);
+            var semanticModel = await document.GetSemanticModelAsync(context.CancellationToken)
+                                           .ConfigureAwait(false);
             foreach (var diagnostic in context.Diagnostics)
             {
                 var token = syntaxRoot.FindToken(diagnostic.Location.SourceSpan.Start);
@@ -34,31 +33,17 @@ namespace WpfAnalyzers
                     continue;
                 }
 
-                var member = syntaxRoot.FindNode(diagnostic.Location.SourceSpan)
-                                       .FirstAncestorOrSelf<MemberDeclarationSyntax>();
-                if (member != null)
+                if (syntaxRoot.TryFindNode(diagnostic, out MemberDeclarationSyntax member) &&
+                    semanticModel.TryGetSymbol(member, context.CancellationToken, out ISymbol symbol) &&
+                    BackingFieldOrProperty.TryCreate(symbol, out var fieldOrProperty) &&
+                    DependencyProperty.TryGetRegisteredName(fieldOrProperty, semanticModel, context.CancellationToken, out var name))
                 {
                     context.RegisterCodeFix(
-                        "Add xml documentation.",
-                        (editor, cancellationToken) => AddDocumentation(editor, member, cancellationToken),
+                        "Add standard documentation.",
+                        (editor, _) => editor.ReplaceNode(member, x => x.WithDocumentationText($"/// <summary>Identifies the <see cref=\"{name}\"/> dependency property.</summary>")),
                         this.GetType(),
                         diagnostic);
                 }
-            }
-        }
-
-        private static void AddDocumentation(DocumentEditor editor, MemberDeclarationSyntax member, CancellationToken cancellationToken)
-        {
-            var symbol = editor.SemanticModel.GetDeclaredSymbolSafe(member, cancellationToken);
-            if (BackingFieldOrProperty.TryCreate(symbol, out var fieldOrProperty) &&
-                DependencyProperty.TryGetRegisteredName(fieldOrProperty, editor.SemanticModel, cancellationToken, out var name))
-            {
-                editor.ReplaceNode(
-                    member,
-                    member.WithLeadingTrivia(
-                        member.GetLeadingTrivia()
-                              .AddRange(SyntaxFactory.ParseLeadingTrivia($"/// <summary>Identifies the <see cref=\"{name}\"/> dependency property.</summary>"))
-                              .Add(SyntaxFactory.ElasticLineFeed)));
             }
         }
     }
