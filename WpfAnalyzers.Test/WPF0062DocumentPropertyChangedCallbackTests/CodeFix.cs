405dec287945aa70bac98f9a2269f7f961b5132a
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-15 14:41:41 +0200

    More tests.

--- WpfAnalyzers.Test/WPF0062DocumentPropertyChangedCallbackTests/CodeFix.cs
+++ WpfAnalyzers.Test/WPF0062DocumentPropertyChangedCallbackTests/CodeFix.cs
@@ -114,6 +114,102 @@ namespace RoslynSandbox
         }
 
         [Test]
+        public void DependencyPropertyRegisterWithMetadataDifferentParameternames()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class FooControl : Control
+    {
+        public static readonly DependencyProperty BarProperty = DependencyProperty.Register(
+            nameof(Bar),
+            typeof(int),
+            typeof(FooControl),
+            new PropertyMetadata(default(int), OnBarChanged));
+
+        public int Bar
+        {
+            get => (int)this.GetValue(BarProperty);
+            set => this.SetValue(BarProperty, value);
+        }
+
+        protected virtual void ↓OnBarChanged(object oldBar, object newBar)
+        {
+        }
+
+        private static void OnBarChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (FooControl)d;
+            control.OnBarChanged(e.OldValue, e.NewValue);
+        }
+
+        private static object CoerceBar(DependencyObject d, object baseValue)
+        {
+            if (baseValue is int i &&
+                i < 0)
+            {
+                return 0;
+            }
+
+            return baseValue;
+        }
+    }
+}";
+
+            var fixedCode = @"
+namespace RoslynSandbox
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class FooControl : Control
+    {
+        public static readonly DependencyProperty BarProperty = DependencyProperty.Register(
+            nameof(Bar),
+            typeof(int),
+            typeof(FooControl),
+            new PropertyMetadata(default(int), OnBarChanged));
+
+        public int Bar
+        {
+            get => (int)this.GetValue(BarProperty);
+            set => this.SetValue(BarProperty, value);
+        }
+
+        /// <summary>This method is invoked when the <see cref=""BarProperty""/> changes.</summary>
+        /// <param name=""oldBar"">The old value of <see cref=""BarProperty""/>.</param>
+        /// <param name=""newBar"">The new value of <see cref=""BarProperty""/>.</param>
+        protected virtual void OnBarChanged(object oldBar, object newBar)
+        {
+        }
+
+        private static void OnBarChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (FooControl)d;
+            control.OnBarChanged(e.OldValue, e.NewValue);
+        }
+
+        private static object CoerceBar(DependencyObject d, object baseValue)
+        {
+            if (baseValue is int i &&
+                i < 0)
+            {
+                return 0;
+            }
+
+            return baseValue;
+        }
+    }
+}";
+
+            AnalyzerAssert.Valid(Analyzer, fixedCode);
+            AnalyzerAssert.CodeFix(Analyzer, Fix, ExpectedDiagnostic, testCode, fixedCode);
+        }
+
+        [Test]
         public void DependencyPropertyRegisterMissingDocsOldValueAndNewValue()
         {
             var testCode = @"
