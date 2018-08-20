c174aa4f44a41a8297d0605d570fe7f412597a0f
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-20 08:41:40 +0200

    More happy path tests for WPF007

--- WpfAnalyzers.Test/WPF0007ValidateValueCallbackCallbackShouldMatchRegisteredNameTests/HappyPath.cs
+++ WpfAnalyzers.Test/WPF0007ValidateValueCallbackCallbackShouldMatchRegisteredNameTests/HappyPath.cs
@@ -8,6 +8,65 @@ namespace WpfAnalyzers.Test.WPF0007ValidateValueCallbackCallbackShouldMatchRegis
         private static readonly RegistrationAnalyzer Analyzer = new RegistrationAnalyzer();
 
         [Test]
+        public void WhenValidationMethodInHelperClass()
+        {
+            var validationCode = @"
+namespace RoslynSandbox
+{
+    internal static class CommonValidation
+    {
+        public static bool ValidateDoubleIsGreaterThanZero(object value)
+        {
+            if (value is double d)
+            {
+                return d > 0;
+            }
+
+            return false;
+        }
+    }
+}";
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class FooControl : Control
+    {
+        /// <summary>Identifies the <see cref=""Bar""/> dependency property.</summary>
+        public static readonly DependencyProperty BarProperty = DependencyProperty.Register(
+            nameof(Bar),
+            typeof(double),
+            typeof(FooControl),
+            new FrameworkPropertyMetadata(1d),
+            CommonValidation.ValidateDoubleIsGreaterThanZero);
+
+        /// <summary>Identifies the <see cref=""Baz""/> dependency property.</summary>
+        public static readonly DependencyProperty BazProperty = DependencyProperty.Register(
+            nameof(Baz),
+            typeof(double),
+            typeof(FooControl),
+            new FrameworkPropertyMetadata(1d),
+            CommonValidation.ValidateDoubleIsGreaterThanZero);
+
+        public double Bar
+        {
+            get => (double)this.GetValue(BarProperty);
+            set => this.SetValue(BarProperty, value);
+        }
+
+        public double Baz
+        {
+            get => (double)this.GetValue(BazProperty);
+            set => this.SetValue(BazProperty, value);
+        }
+    }
+}";
+            AnalyzerAssert.Valid(Analyzer, validationCode, testCode);
+        }
+
+        [Test]
         public void DependencyPropertyNoCallback()
         {
             var testCode = @"
