a095c3878eb552438ede849f2fd5c754372ef536
JohanLarsson <johan_gavle1@hotmail.com>
2018-09-03 14:49:02 +0200

    More tests.

--- WpfAnalyzers.Test/WPF0131TemplatePartTypeTests/ValidCode.cs
+++ WpfAnalyzers.Test/WPF0131TemplatePartTypeTests/ValidCode.cs
@@ -9,7 +9,7 @@ namespace WpfAnalyzers.Test.WPF0131TemplatePartTypeTests
         private static readonly DiagnosticAnalyzer Analyzer = new GetTemplateChildAnalyzer();
 
         [Test]
-        public void ExactMatch()
+        public void WhenCastingToSameType()
         {
             var testCode = @"
 namespace RoslynSandbox
@@ -24,6 +24,27 @@ namespace RoslynSandbox
         {
             base.OnApplyTemplate();
             var bar = (Border)this.GetTemplateChild(""PART_Bar"");
+        }
+    }
+}";
+            AnalyzerAssert.Valid(Analyzer, testCode);
+        }
+
+        [Test]
+        public void WhenCastingToSameTypeFullyQualified()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System.Windows;
+
+    [TemplatePart(Name = ""PART_Bar"", Type = typeof(System.Windows.Controls.Border))]
+    public class FooControl : System.Windows.Controls.Control
+    {
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var bar = (System.Windows.Controls.Border)this.GetTemplateChild(""PART_Bar"");
         }
     }
 }";
