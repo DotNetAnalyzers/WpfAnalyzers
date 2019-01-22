43d4a1cbb7ae04226150d4d23a8ca55a365bea2c
JohanLarsson <johan_gavle1@hotmail.com>
2019-01-22 12:13:00 +0100

    Amend.

--- WpfAnalyzers.Test/AssemblyAttributes.cs
+++ WpfAnalyzers.Test/AssemblyAttributes.cs
@@ -1,21 +1,7 @@
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using Gu.Roslyn.Asserts;
 
-[assembly: AssemblyTitle("WpfAnalyzers.Test")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: CLSCompliant(false)]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0-dev")]
 [assembly: MetadataReference(typeof(object), new[] { "global", "mscorlib" })]
 [assembly: MetadataReference(typeof(System.Diagnostics.Debug), new[] { "global", "System" })]
 [assembly: TransitiveMetadataReferences(typeof(Microsoft.CodeAnalysis.CSharp.CSharpCompilation))]
