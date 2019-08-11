e5be431cae7483998a13a9a1e6f6c881674fe92e
Johan Larsson <johan_gavle1@hotmail.com>
2019-08-11 14:11:02 +0200

    Use MetadataReferences in benchmark.

--- WpfAnalyzers.Benchmarks/Benchmarks/CodeGen/Code.cs
+++ WpfAnalyzers.Benchmarks/Benchmarks/CodeGen/Code.cs
@@ -2,27 +2,13 @@ namespace WpfAnalyzers.Benchmarks.Benchmarks
 {
     using System;
     using System.Collections.Generic;
-    using System.Collections.Immutable;
     using System.Linq;
-    using System.Net;
     using Gu.Roslyn.Asserts;
     using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.Diagnostics;
 
     public static class Code
     {
-        private static readonly IReadOnlyList<MetadataReference> MetadataReferences = new[]
-        {
-            MetadataReference.CreateFromFile(typeof(object).Assembly.Location).WithAliases(ImmutableArray.Create("global", "corlib")),
-            MetadataReference.CreateFromFile(typeof(System.Diagnostics.Debug).Assembly.Location).WithAliases(ImmutableArray.Create("global", "system")),
-            MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location),
-            MetadataReference.CreateFromFile(typeof(WebClient).Assembly.Location),
-            MetadataReference.CreateFromFile(typeof(System.Xml.Serialization.XmlSerializer).Assembly.Location),
-            MetadataReference.CreateFromFile(typeof(CSharpCompilation).Assembly.Location),
-            MetadataReference.CreateFromFile(typeof(Compilation).Assembly.Location),
-        };
-
         private static readonly IReadOnlyList<DiagnosticAnalyzer> AllAnalyzers = typeof(KnownSymbol).Assembly
                                                                                                     .GetTypes()
                                                                                                     .Where(typeof(DiagnosticAnalyzer).IsAssignableFrom)
@@ -32,6 +18,6 @@ namespace WpfAnalyzers.Benchmarks.Benchmarks
         public static Solution ValidCodeProject { get; } = CodeFactory.CreateSolution(
             ProjectFile.Find("ValidCode.csproj"),
             AllAnalyzers,
-            MetadataReferences);
+            MetadataReferences.Transitive(typeof(System.Windows.Window)));
     }
 }
