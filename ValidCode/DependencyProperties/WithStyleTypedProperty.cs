2c61a3a84cbcd5dddfcf029a62d8d14aff56aae0
JohanLarsson <johan_gavle1@hotmail.com>
2019-08-12 17:32:03 +0200

    WithStyleTypedProperty in ValidCode.csproj
    #242

---
+++ ValidCode/DependencyProperties/WithStyleTypedProperty.cs
@@ -0,0 +1,22 @@
+namespace ValidCode.DependencyProperties
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    [StyleTypedProperty(Property = nameof(BarStyle), StyleTargetType = typeof(Control))]
+    public class WithStyleTypedProperty : Control
+    {
+        /// <summary>Identifies the <see cref="BarStyle"/> dependency property.</summary>
+        public static readonly DependencyProperty BarStyleProperty = DependencyProperty.Register(
+            nameof(BarStyle),
+            typeof(Style),
+            typeof(WithStyleTypedProperty),
+            new PropertyMetadata(default(Style)));
+
+        public Style BarStyle
+        {
+            get => (Style)this.GetValue(BarStyleProperty);
+            set => this.SetValue(BarStyleProperty, value);
+        }
+    }
+}
