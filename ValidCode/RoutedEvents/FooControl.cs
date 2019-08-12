05c803e3331eb5900444eb6c81b67cc1202d0a41
JohanLarsson <johan_gavle1@hotmail.com>
2019-08-12 09:46:23 +0200

    More valid code.

---
+++ ValidCode/RoutedEvents/FooControl.cs
@@ -0,0 +1,21 @@
+namespace ValidCode.RoutedEvents
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class FooControl : Control
+    {
+        /// <summary>Identifies the ValueChanged event</summary>
+        public static readonly RoutedEvent ValueChangedEvent = EventManager.RegisterRoutedEvent(
+            nameof(ValueChanged),
+            RoutingStrategy.Direct,
+            typeof(RoutedEventHandler),
+            typeof(FooControl));
+
+        public event RoutedEventHandler ValueChanged
+        {
+            add { this.AddHandler(ValueChangedEvent, value); }
+            remove { this.RemoveHandler(ValueChangedEvent, value); }
+        }
+    }
+}
