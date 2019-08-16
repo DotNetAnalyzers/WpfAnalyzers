a83ad67ab2bc63c7386f6bfa0211ead5262f4c90
JohanLarsson <johan_gavle1@hotmail.com>
2019-08-16 10:04:17 +0200

    Simplify FixCastCodeFixProvider

--- WpfAnalyzers/CodeFixes/FixCastCodeFixProvider.cs
+++ WpfAnalyzers/CodeFixes/FixCastCodeFixProvider.cs
@@ -8,7 +8,6 @@ namespace WpfAnalyzers
     using Microsoft.CodeAnalysis.CodeFixes;
     using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
-    using Microsoft.CodeAnalysis.Editing;
 
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(FixCastCodeFixProvider))]
     [Shared]
@@ -29,64 +28,16 @@ namespace WpfAnalyzers
                                            .ConfigureAwait(false);
             foreach (var diagnostic in context.Diagnostics)
             {
-                var token = syntaxRoot.FindToken(diagnostic.Location.SourceSpan.Start);
-                if (string.IsNullOrEmpty(token.ValueText))
+                if (syntaxRoot.TryFindNode(diagnostic, out TypeSyntax typeSyntax) &&
+                    diagnostic.Properties.TryGetValue("ExpectedType", out var registeredType))
                 {
-                    continue;
-                }
-
-                if (diagnostic.Properties.TryGetValue("ExpectedType", out var registeredType))
-                {
-                    var node = syntaxRoot.FindNode(diagnostic.Location.SourceSpan, getInnermostNodeForTie: true);
-                    if (node.FirstAncestorOrSelf<IdentifierNameSyntax>() is IdentifierNameSyntax identifierName &&
-                        !identifierName.IsMissing)
-                    {
-                        context.RegisterCodeFix(
-                            $"Change type to: {registeredType}.",
-                            (e, _) => ChangeType(e, identifierName, registeredType),
-                            this.GetType().FullName,
-                            diagnostic);
-                    }
-
-                    if (node.FirstAncestorOrSelf<PredefinedTypeSyntax>() is PredefinedTypeSyntax predefinedType &&
-                        !predefinedType.IsMissing)
-                    {
-                        context.RegisterCodeFix(
-                            $"Change type to: {registeredType}.",
-                            (e, _) => ChangeType(e, predefinedType, registeredType),
-                            this.GetType().FullName,
-                            diagnostic);
-                    }
-
-                    if (node.FirstAncestorOrSelf<QualifiedNameSyntax>() is QualifiedNameSyntax qualifiedName &&
-                        !qualifiedName.IsMissing)
-                    {
-                        context.RegisterCodeFix(
-                            $"Change type to: {registeredType}.",
-                            (e, _) => e.ReplaceNode(
-                                qualifiedName,
-#pragma warning disable SA1313 // Parameter names must begin with lower-case letter
-                                (x, __) => SyntaxFactory.ParseTypeName(registeredType)),
-#pragma warning restore SA1313 // Parameter names must begin with lower-case letter
-                            this.GetType().FullName,
-                            diagnostic);
-                    }
+                    context.RegisterCodeFix(
+                        $"Change type to: {registeredType}.",
+                        (e, _) => e.ReplaceNode(typeSyntax, SyntaxFactory.ParseTypeName(registeredType)),
+                        this.GetType().FullName,
+                        diagnostic);
                 }
             }
-        }
-
-        private static void ChangeType(DocumentEditor editor, PredefinedTypeSyntax typeSyntax, string newType)
-        {
-            editor.ReplaceNode(
-                typeSyntax,
-                (x, _) => SyntaxFactory.ParseTypeName(newType));
-        }
-
-        private static void ChangeType(DocumentEditor editor, IdentifierNameSyntax typeSyntax, string newType)
-        {
-            editor.ReplaceNode(
-                typeSyntax,
-                (x, _) => ((IdentifierNameSyntax)x).WithIdentifier(SyntaxFactory.ParseToken(newType)));
         }
     }
 }
