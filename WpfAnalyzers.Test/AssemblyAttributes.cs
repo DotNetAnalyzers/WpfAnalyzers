0a609e1bc8aaeb1a6fd30a4c040209ddc98945ca
Johan Larsson <johan_gavle1@hotmail.com>
2019-11-02 08:13:08 +0100

    Use nuget.

--- WpfAnalyzers.Test/AssemblyAttributes.cs
+++ WpfAnalyzers.Test/AssemblyAttributes.cs
@@ -1,18 +1,3 @@
 using Gu.Roslyn.Asserts;
 
-[assembly: MetadataReference(typeof(object), new[] { "global", "mscorlib" })]
-[assembly: MetadataReference(typeof(System.Diagnostics.Debug), new[] { "global", "System" })]
-[assembly: TransitiveMetadataReferences(typeof(Microsoft.CodeAnalysis.CSharp.CSharpCompilation))]
-[assembly: TransitiveMetadataReferences(typeof(Gu.Roslyn.CodeFixExtensions.DocumentEditorCodeFixProvider))]
-[assembly: MetadataReferences(
-    typeof(System.Linq.Enumerable),
-    typeof(System.Net.WebClient),
-    typeof(System.Drawing.Bitmap),
-    typeof(System.Data.Common.DbConnection),
-    typeof(System.Xml.Serialization.XmlSerializer),
-    typeof(System.Runtime.Serialization.DataContractSerializer),
-    typeof(System.Windows.Media.Brush),
-    typeof(System.Windows.Controls.Control),
-    typeof(System.Windows.Media.Matrix),
-    typeof(System.Xaml.XamlLanguage),
-    typeof(NUnit.Framework.Assert))]
+[assembly: TransitiveMetadataReferences(typeof(System.Windows.Controls.Button))]
