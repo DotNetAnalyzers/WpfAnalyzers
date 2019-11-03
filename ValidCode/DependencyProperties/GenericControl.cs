e6c091c40bed2e6d3b3b4fdb7e141cfb495dd6e0
Johan Larsson <johan_gavle1@hotmail.com>
2019-11-03 08:13:16 +0100

    More valid code.

--- ValidCode/DependencyProperties/GenericControl.cs
+++ ValidCode/DependencyProperties/GenericControl.cs
@@ -6,22 +6,44 @@ namespace ValidCode.DependencyProperties
 
     public class GenericControl<T> : Control
     {
-        /// <summary>Identifies the <see cref="Bar"/> dependency property.</summary>
-        public static readonly DependencyProperty BarProperty = DependencyProperty.Register(
-            nameof(Bar),
+        /// <summary>Identifies the <see cref="Generic"/> dependency property.</summary>
+        public static readonly DependencyProperty GenericProperty = DependencyProperty.Register(
+            nameof(Generic),
             typeof(T),
             typeof(GenericControl<T>),
             new PropertyMetadata(
                 default(T),
-                OnBarChanged));
+                OnGenericChanged));
 
-        public T Bar
+        public static readonly DependencyProperty GenericAttachedProperty = DependencyProperty.RegisterAttached(
+            "GenericAttached", 
+            typeof(T), 
+            typeof(GenericControl<T>),
+            new PropertyMetadata(default(T)));
+
+        public T Generic
         {
-            get => (T)this.GetValue(BarProperty);
-            set => this.SetValue(BarProperty, value);
+            get => (T)this.GetValue(GenericProperty);
+            set => this.SetValue(GenericProperty, value);
         }
 
-        private static void OnBarChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        /// <summary>Helper for setting <see cref="GenericAttachedProperty"/> on <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="DependencyObject"/> to set <see cref="GenericAttachedProperty"/> on.</param>
+        /// <param name="value">GenericAttached property value.</param>
+        public static void SetGenericAttached(DependencyObject element, T value)
+        {
+            element.SetValue(GenericAttachedProperty, value);
+        }
+
+        /// <summary>Helper for getting <see cref="GenericAttachedProperty"/> from <paramref name="element"/>.</summary>
+        /// <param name="element"><see cref="DependencyObject"/> to read <see cref="GenericAttachedProperty"/> from.</param>
+        /// <returns>GenericAttached property value.</returns>
+        public static T GetGenericAttached(DependencyObject element)
+        {
+            return (T)element.GetValue(GenericAttachedProperty);
+        }
+
+        private static void OnGenericChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var value = (T)e.NewValue;
         }
---
+++ ValidCode/DependencyProperties/StringControl.cs
@@ -0,0 +1,21 @@
+// ReSharper disable All
+namespace ValidCode.DependencyProperties
+{
+    using System.Windows;
+
+    public class StringControl : GenericControl<string>
+    {
+        /// <summary>Identifies the <see cref="Text"/> dependency property.</summary>
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            nameof(Text), 
+            typeof(string), 
+            typeof(StringControl),
+            new PropertyMetadata(default(string)));
+
+        public string Text
+        {
+            get => (string)this.GetValue(TextProperty);
+            set => this.SetValue(TextProperty, value);
+        }
+    }
+}
