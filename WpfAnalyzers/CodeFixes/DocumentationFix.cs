ef828355979531b4a67c0c873acd7fc85d69e3b4
JohanLarsson <johan_gavle1@hotmail.com>
2019-11-21 16:31:55 +0100

    Update dependencies.

--- WpfAnalyzers/CodeFixes/DocumentationFix.cs
+++ WpfAnalyzers/CodeFixes/DocumentationFix.cs
@@ -15,8 +15,11 @@
     [Shared]
     internal class DocumentationFix : DocumentEditorCodeFixProvider
     {
+        private const string Title = "Add standard documentation.";
+
         public override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create(
             Descriptors.WPF0060DocumentDependencyPropertyBackingMember.Id,
+            Descriptors.WPF0061DocumentClrMethod.Id,
             Descriptors.WPF0062DocumentPropertyChangedCallback.Id);
 
         protected override async Task RegisterCodeFixesAsync(DocumentEditorCodeFixContext context)
@@ -31,76 +34,74 @@
                     {
                         case XmlElementSyntax element:
                             context.RegisterCodeFix(
-                                "Add standard documentation.",
-                                (editor, _) => editor.ReplaceNode(element, x => Parse.XmlElementSyntax(text)),
-                                this.GetType(),
+                                Title,
+                                editor => editor.ReplaceNode(element, x => Parse.XmlElementSyntax(text)),
+                                Title,
                                 diagnostic);
                             break;
                         case XmlEmptyElementSyntax element:
                             context.RegisterCodeFix(
-                                "Add standard documentation.",
-                                (editor, _) => editor.ReplaceNode(element, x => Parse.XmlEmptyElementSyntax(text)),
-                                this.GetType(),
+                                Title,
+                                editor => editor.ReplaceNode(element, x => Parse.XmlEmptyElementSyntax(text)),
+                                Title,
                                 diagnostic);
                             break;
                         case XmlNameSyntax name:
                             context.RegisterCodeFix(
-                                "Add standard documentation.",
-                                (editor, _) => editor.ReplaceNode(name, x => SyntaxFactory.XmlName(text)),
-                                this.GetType(),
+                                Title,
+                                editor => editor.ReplaceNode(name, x => SyntaxFactory.XmlName(text)),
+                                Title,
+                                diagnostic);
+                            break;
+                        case DocumentationCommentTriviaSyntax element
+                            when text.StartsWith("<summary>"):
+                            context.RegisterCodeFix(
+                                Title,
+                                editor => editor.ReplaceNode(element, x => x.WithSummary(Parse.XmlElementSyntax(text))),
+                                Title,
                                 diagnostic);
                             break;
                         case DocumentationCommentTriviaSyntax element:
                             context.RegisterCodeFix(
-                                "Add standard documentation.",
-                                (editor, __) =>
-                                {
-                                    if (text.StartsWith("<summary>"))
-                                    {
-                                        _ = editor.ReplaceNode(element, x => x.WithSummary(Parse.XmlElementSyntax(text)));
-                                    }
-                                    else
-                                    {
-                                        _ = editor.ReplaceNode(element, x => Parse.DocumentationCommentTriviaSyntax(text));
-                                    }
-                                },
-                                this.GetType(),
+                                Title,
+                                editor => editor.ReplaceNode(element, x => Parse.DocumentationCommentTriviaSyntax(text)),
+                                Title,
                                 diagnostic);
                             break;
                         case IdentifierNameSyntax identifierName:
                             context.RegisterCodeFix(
-                                "Add standard documentation.",
-                                (editor, _) => editor.ReplaceNode(identifierName, x => x.WithIdentifier(SyntaxFactory.Identifier(text))),
-                                this.GetType(),
+                                Title,
+                                editor => editor.ReplaceNode(identifierName, x => x.WithIdentifier(SyntaxFactory.Identifier(text))),
+                                Title,
                                 diagnostic);
                             break;
                         case MethodDeclarationSyntax method:
                             context.RegisterCodeFix(
-                                "Add standard documentation.",
-                                (editor, _) => editor.ReplaceNode(method, x => x.WithDocumentationText(text)),
-                                this.GetType(),
+                                Title,
+                                editor => editor.ReplaceNode(method, x => x.WithDocumentationText(text)),
+                                Title,
                                 diagnostic);
                             break;
                         case PropertyDeclarationSyntax property:
                             context.RegisterCodeFix(
-                                "Add standard documentation.",
-                                (editor, _) => editor.ReplaceNode(property, x => x.WithDocumentationText(text)),
-                                this.GetType(),
+                                Title,
+                                editor => editor.ReplaceNode(property, x => x.WithDocumentationText(text)),
+                                Title,
                                 diagnostic);
                             break;
                         case VariableDeclaratorSyntax { Parent: VariableDeclarationSyntax { Parent: FieldDeclarationSyntax field } }:
                             context.RegisterCodeFix(
-                                "Add standard documentation.",
-                                (editor, _) => editor.ReplaceNode(field, x => x.WithDocumentationText(text)),
-                                this.GetType(),
+                                Title,
+                                editor => editor.ReplaceNode(field, x => x.WithDocumentationText(text)),
+                                Title,
                                 diagnostic);
                             break;
                         case ParameterSyntax { Parent: ParameterListSyntax { Parent: MethodDeclarationSyntax method } } parameter
                             when method.TryGetDocumentationComment(out var comment):
                             context.RegisterCodeFix(
-                                "Add standard documentation.",
-                                (editor, _) => editor.ReplaceNode(comment, x => x.WithParamText(parameter.Identifier.ValueText, text)),
-                                this.GetType(),
+                                Title,
+                                editor => editor.ReplaceNode(comment, x => x.WithParamText(parameter.Identifier.ValueText, text)),
+                                Title,
                                 diagnostic);
                             break;
                         case { } node:
