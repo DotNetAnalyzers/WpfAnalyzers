5d90f32297c6d791e13c35b3f9d0bd669e8c293e
JohanLarsson <johan_gavle1@hotmail.com>
2019-11-30 11:42:31 +0100

    Add UsingBoolBoxes to ValidCode.csproj

---
+++ ValidCode/DependencyProperties/UsingBoolBoxes.cs
@@ -0,0 +1,21 @@
+﻿namespace ValidCode.DependencyProperties
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class UsingBoolBoxes : Control
+    {
+        /// <summary>Identifies the <see cref="IsTrue"/> dependency property.</summary>
+        public static readonly DependencyProperty IsTrueProperty = DependencyProperty.Register(
+            nameof(IsTrue),
+            typeof(bool),
+            typeof(UsingBoolBoxes),
+            new PropertyMetadata(BooleanBoxes.False));
+
+        public bool IsTrue
+        {
+            get => Equals(BooleanBoxes.True, this.GetValue(IsTrueProperty));
+            set => this.SetValue(IsTrueProperty, BooleanBoxes.Box(value));
+        }
+    }
+}
