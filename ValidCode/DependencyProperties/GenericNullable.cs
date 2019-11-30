927f8dd0f1c56fa9a0121e53c6063e6f8375289c
JohanLarsson <johan_gavle1@hotmail.com>
2019-11-30 11:35:56 +0100

    Add GenericNullable in valid code.

---
+++ ValidCode/DependencyProperties/GenericNullable.cs
@@ -0,0 +1,33 @@
+﻿// ReSharper disable All
+namespace ValidCode.DependencyProperties
+{
+    using System.Windows;
+
+    public class GenericNullable<T> : FrameworkElement
+        where T : struct
+    {
+        /// <summary>Identifies the <see cref="Value"/> dependency property.</summary>
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(Value),
+            typeof(T?),
+            typeof(GenericNullable<T>),
+            new PropertyMetadata(default(T?), OnValueChanged));
+
+        public T? Value
+        {
+            get { return (T?)this.GetValue(ValueProperty); }
+            set { this.SetValue(ValueProperty, value); }
+        }
+
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.OldValue is T oldValue)
+            {
+            }
+
+            if (e.NewValue is T newValue)
+            {
+            }
+        }
+    }
+}
