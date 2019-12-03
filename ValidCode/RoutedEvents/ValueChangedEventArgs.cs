59ec36ebc0eacf4e952f6ad2bfd8449bbc11ebfc
JohanLarsson <johan_gavle1@hotmail.com>
2019-12-03 12:16:25 +0100

    More valid code.

---
+++ ValidCode/RoutedEvents/ValueChangedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿namespace ValidCode.RoutedEvents
+{
+    using System.Windows;
+
+    public delegate void ValueChangedEventHandler(object sender, ValueChangedEventArgs e);
+
+    public class ValueChangedEventArgs : RoutedEventArgs
+    {
+        public ValueChangedEventArgs(double oldValue, double newValue)
+        {
+            this.OldValue = oldValue;
+            this.NewValue = newValue;
+        }
+
+        public double OldValue { get; }
+
+        public double NewValue { get; }
+    }
+}
---
+++ ValidCode/RoutedEvents/WithCustomEventHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace ValidCode.RoutedEvents
+{
+    using System.ComponentModel;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class WithCustomEventHandler : Control
+    {
+        /// <summary>Identifies the <see cref="ValueChanged"/> routed event.</summary>
+        public static readonly RoutedEvent ValueChangedEvent = EventManager.RegisterRoutedEvent(
+            nameof(ValueChanged),
+            RoutingStrategy.Direct,
+            typeof(ValueChangedEventHandler),
+            typeof(WithCustomEventHandler));
+
+        [Browsable(true)]
+        public event ValueChangedEventHandler ValueChanged
+        {
+            add => this.AddHandler(ValueChangedEvent, value);
+            remove => this.RemoveHandler(ValueChangedEvent, value);
+        }
+    }
+}
