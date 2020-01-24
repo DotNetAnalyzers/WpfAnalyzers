3d379d39e0198f2bb571f697e1160321e198a062
JohanLarsson <johan_gavle1@hotmail.com>
2020-01-24 08:35:32 +0100

    Explicit references to Microsoft.CodeAnalysis.CSharp.Workspaces

--- WpfAnalyzers/CodeFixes/Helpers/RenameHelper.cs
+++ WpfAnalyzers/CodeFixes/Helpers/RenameHelper.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 namespace WpfAnalyzers
