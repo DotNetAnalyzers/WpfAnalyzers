788ae1ac54dcd912ec3b1cfdf8c0f81cd7c08953
JohanLarsson <johan_gavle1@hotmail.com>
2020-05-15 11:11:49 +0200

    Fix #277

---
+++ ValidCode/Issues/Issue277.cs
@@ -0,0 +1,28 @@
+﻿namespace ValidCode.Issues.N
+{
+    using System.Windows;
+    using AliasedType = AliasedType;
+
+    public class Issue277 : FrameworkElement
+    {
+        /// <summary>Identifies the <see cref="AliasedType"/> dependency property.</summary>
+        public static readonly DependencyProperty AliasedTypeProperty = DependencyProperty.Register(
+            nameof(AliasedType),
+            typeof(AliasedType),
+            typeof(Issue277),
+            new PropertyMetadata(default(AliasedType)));
+
+        public AliasedType AliasedType
+        {
+            get => (AliasedType)this.GetValue(AliasedTypeProperty);
+            set => this.SetValue(AliasedTypeProperty, value);
+        }
+    }
+}
+
+namespace ValidCode.Issues
+{
+    public class AliasedType
+    {
+    }
+}
