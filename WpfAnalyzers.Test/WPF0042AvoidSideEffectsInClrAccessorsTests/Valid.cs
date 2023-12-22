4a9ff0d98559bd0edeb4779d9d5d047bb6a36e96
LaniusExcubitor <marvin@dodgethis.de>
2023-12-22 10:40:41 +0100

    fix ArgumentNullException.ThrowIfNull

--- WpfAnalyzers.Test/WPF0042AvoidSideEffectsInClrAccessorsTests/Valid.cs
+++ WpfAnalyzers.Test/WPF0042AvoidSideEffectsInClrAccessorsTests/Valid.cs
@@ -359,6 +359,51 @@ namespace N
         RoslynAssert.Valid(Analyzer, code);
     }
 
+
+    [Test]
+    public static void AttachedPropertyWithNewNullChecks()
+    {
+        var code = @"
+namespace N
+{
+    using System;
+    using System.Windows;
+
+    public static class WithNullChecks
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.RegisterAttached(
+            ""Text"",
+            typeof(string),
+            typeof(WithNullChecks),
+            new PropertyMetadata(default(string)));
+
+        /// <summary>Helper for setting <see cref=""TextProperty""/> on <paramref name=""element""/>.</summary>
+        /// <param name=""element""><see cref=""DependencyObject""/> to set <see cref=""TextProperty""/> on.</param>
+        /// <param name=""value"">Text property value.</param>
+        public static void SetText(DependencyObject element, string? value)
+        {
+            ArgumentNullException.ThrowIfNull(element);
+
+            element.SetValue(TextProperty, value);
+        }
+
+        /// <summary>Helper for getting <see cref=""TextProperty""/> from <paramref name=""element""/>.</summary>
+        /// <param name=""element""><see cref=""DependencyObject""/> to read <see cref=""TextProperty""/> from.</param>
+        /// <returns>Text property value.</returns>
+        [AttachedPropertyBrowsableForType(typeof(DependencyObject))]
+        public static string? GetText(DependencyObject element)
+        {
+            ArgumentNullException.ThrowIfNull(element);
+
+            return (string?)element.GetValue(TextProperty);
+        }
+    }
+}";
+
+        RoslynAssert.Valid(Analyzer, code);
+    }
+
+
     [Test]
     public static void AttachedPropertyWithEmptyIfs()
     {
